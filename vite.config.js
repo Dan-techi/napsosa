0a0be813ce81895c1dd3c6c24e2d06628879f6a4
Dan-techi <danmugume24@gmail.com>
2026-06-19 11:11:46 +0300

    fix: update base path to napsosa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/naps/',
+  base: '/napsosa/',
   build: {
     outDir: 'dist',
     sourcemap: false,
